f8c27a509127350eb31752657e66af3533915162
achristoforou1 <a_christoforou1@yahoo.com>
2025-06-03 20:41:25 +0300

    deployment settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+import dj_database_url 
+if os.path.isfile('env.py'):
+    import env
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +28,8 @@ SECRET_KEY = 'django-insecure-3k*u@s%n3f)o(vl^dw-rx91&n9$73bppbbo%!w)-%3b7h^oyan
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [,'.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
+    
 
 
 # Application definition
@@ -73,11 +77,16 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+#DATABASES = {
+ #   'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+ #      'NAME': BASE_DIR / 'db.sqlite3',
+  #  }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
--- codestar/wsgi.py
+++ codestar/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import env
+
 
 from django.core.wsgi import get_wsgi_application
 
